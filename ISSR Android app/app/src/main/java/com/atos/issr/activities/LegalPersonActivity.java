44ce7b0a38ec905d777cfccf1e78a250e3fb4c59
Jaroslav Hladik <jaroslav.hladik@atos.net>
2020-06-03 07:51:18 +0200

    updated activity for legal person

--- ISSR Android app/app/src/main/java/com/atos/issr/activities/LegalPersonActivity.java
+++ ISSR Android app/app/src/main/java/com/atos/issr/activities/LegalPersonActivity.java
@@ -2,14 +2,40 @@ package com.atos.issr.activities;
 
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.RadioButton;
 
 import com.atos.issr.R;
 
 public class LegalPersonActivity extends BaseActivity {
+    public static final String TAG = "LegalPersonActivity";
+
+    private int covidState;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_leagal_person);
+    }
+
+    public void onRadioButtonClicked(View view) {
+        boolean checked = ((RadioButton) view).isChecked();
+
+        switch (view.getId()) {
+            case R.id.covid_state1:
+                if (checked) {
+                    covidState = 1;
+                    break;
+                }
+            case R.id.covid_state2:
+                if (checked) {
+                    covidState = 2;
+                    break;
+                }
+            default:
+                //nothing
+        }
+        Log.d(TAG, "Covid state changed to state: '"+covidState+'\'');
     }
 }
