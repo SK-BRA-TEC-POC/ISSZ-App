c0389e50db3bc024e37d7e1773c0cc0a96b71c92
Jaroslav Hladik <jaroslav.hladik@atos.net>
2020-06-03 10:41:50 +0200

    activity with detail prepared

---
+++ ISSR Android app/app/src/main/java/com/atos/issr/activities/DetailActivity.java
@@ -0,0 +1,14 @@
+package com.atos.issr.activities;
+
+import android.os.Bundle;
+
+import com.atos.issr.R;
+
+public class DetailActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+    }
+}
